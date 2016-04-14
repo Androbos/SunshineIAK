9cc661c862ad1166c4db1878759b1fb8e29e8520
tommyputranto1 <tommyputranto1@gmail.com>
2016-04-14 23:15:47 +0700

    override method onCreateOptionMenu dan OnCreateOptionSelected

--- app/src/main/java/com/example/tommy/sunshineiak/MainActivity.java
+++ app/src/main/java/com/example/tommy/sunshineiak/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.tommy.sunshineiak;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,4 +17,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }

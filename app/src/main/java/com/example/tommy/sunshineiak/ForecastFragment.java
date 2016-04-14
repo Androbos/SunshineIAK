aaba81f473d8afbd1ade97fd5776f8f5487cd759
tommyputranto1 <tommyputranto1@gmail.com>
2016-04-14 23:28:39 +0700

    update forecastfragment.java agar option menu refresh muncul

--- app/src/main/java/com/example/tommy/sunshineiak/ForecastFragment.java
+++ app/src/main/java/com/example/tommy/sunshineiak/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -33,6 +36,7 @@ public  class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         String[] data = new String[]{
                 "Minggu - hujan - 30/60",
                 "Senin - cerah - 35/67",
@@ -58,6 +62,27 @@ public  class ForecastFragment extends Fragment {
 
         return rootView;
     }
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+       inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.action_refresh){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public class FetchWeatherTask extends AsyncTask<Void , Void, Void>{
 
         @Override

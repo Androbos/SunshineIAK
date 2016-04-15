78ad69ff0257018847a006009131e2dcf586e0d3
tommyputranto1 <tommyputranto1@gmail.com>
2016-04-15 19:25:44 +0700

    buat detailfragment.xml di menu directory dan buat item share

--- app/src/main/java/com/example/tommy/sunshineiak/MainActivity.java
+++ app/src/main/java/com/example/tommy/sunshineiak/MainActivity.java
@@ -32,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings){
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        if (id==R.id.action_map){
             openPreferredLocationInMap();
             return true;
         }

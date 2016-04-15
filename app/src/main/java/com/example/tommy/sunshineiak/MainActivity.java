7213501cf925565cca07e0ec1ff31dd9a5f23470
tommyputranto1 <tommyputranto1@gmail.com>
2016-04-15 19:12:24 +0700

    update di MainActivity menambahkan implicit intent

--- app/src/main/java/com/example/tommy/sunshineiak/MainActivity.java
+++ app/src/main/java/com/example/tommy/sunshineiak/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.tommy.sunshineiak;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -28,10 +32,26 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings){
-            startActivity(new Intent(this, SettingsActivity.class));
+            openPreferredLocationInMap();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void  openPreferredLocationInMap(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPreferences.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default)
+        );
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location).build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager())!= null){
+            startActivity(intent);
+        }else {
+            Log.i("MainActivity","tidak bisa panggil "+ location+", belum menginstall aplikasi maps");
+        }
     }
 }

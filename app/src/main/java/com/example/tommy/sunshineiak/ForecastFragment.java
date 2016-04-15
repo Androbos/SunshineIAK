7f644e172b46459bfed6467b860a4d582c825bd8
tommyputranto1 <tommyputranto1@gmail.com>
2016-04-15 18:50:22 +0700

    update ForecastFragment tambah units listpreferences

--- app/src/main/java/com/example/tommy/sunshineiak/ForecastFragment.java
+++ app/src/main/java/com/example/tommy/sunshineiak/ForecastFragment.java
@@ -110,7 +110,14 @@ public ArrayAdapter<String> adapter;
         /**
          * Prepare the weather high/lows for presentation.
          */
-        private String formatHighLows(double high, double low) {
+        private String formatHighLows(double high, double low, String unitType) {
+            if (unitType.equals(getString(R.string.pref_units_imperial))){
+                high= (high*1.8)+32;
+                low = (low*1.8)+32;
+
+            }else if (!unitType.equals(getString(R.string.pref_units_metric))){
+                Log.i(LOG_TAG, "Unit Type not found: "+ unitType);
+            }
             // For presentation, assume the user doesn't care about tenths of a degree.
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
@@ -158,6 +165,13 @@ public ArrayAdapter<String> adapter;
             dayTime = new Time();
 
             String[] resultStrs = new String[numDays];
+
+            SharedPreferences sharePrefs =
+                    PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = sharePrefs.getString(
+                    getString(R.string.pref_units_key),
+                    getString(R.string.pref_units_metric)
+            );
             for(int i = 0; i < weatherArray.length(); i++) {
                 // For now, using the format "Day, description, hi/low"
                 String day;
@@ -185,7 +199,7 @@ public ArrayAdapter<String> adapter;
                 double high = temperatureObject.getDouble(OWM_MAX);
                 double low = temperatureObject.getDouble(OWM_MIN);
 
-                highAndLow = formatHighLows(high, low);
+                highAndLow = formatHighLows(high, low, unitType);
                 resultStrs[i] = day + " - " + description + " - " + highAndLow;
             }
 
